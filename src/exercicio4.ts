04ed34e10f4b07d762a90c60e5a7c224d2d8716c
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-14 22:13:06 -0300

    exercicio 4 mini

--- src/exercicio4.ts
+++ src/exercicio4.ts
@@ -1,6 +1,4 @@
-function item(entrada:string):string {
-    return `\t<li>${entrada}</li>\n`;
-    }
+const  item = (entrada:string):string => `\t<li>${entrada}</li>\n`; 
 
 function listar(elementos:string[]):string {
     let soma = elementos.reduce(function (total, elemento){
