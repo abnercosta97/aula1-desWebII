a8e73b9c376a8dfa27fffa39c446648d404a6cc0
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-14 21:07:32 -0300

    exercicios 2 e 3

--- src/exercicio2.ts
+++ src/exercicio2.ts
@@ -1,5 +1,4 @@
 import formatar from "./exercicio1";
 const nomes = ["Ana", "Pedro", "Lucas", "Maria"];
-for (let i = 0; i < nomes.length; i++) {
-  console.log(formatar(nomes[i]));
-}
+nomes.forEach((item) => console.log(formatar(item)));
+
