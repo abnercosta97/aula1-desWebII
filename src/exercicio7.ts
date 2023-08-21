4456f4a9f09bfcda513331945e7cb130a5898ae3
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-08-21 20:28:55 -0300

    Exercicio 7 corrigido

--- src/exercicio7.ts
+++ src/exercicio7.ts
@@ -3,7 +3,13 @@ import axios from "axios";
 const cep = "12243750";
 const url = `https://viacep.com.br/ws/${cep}/json`;
 
-axios.get(url)
-.then(res => console.log({res}))
-.catch(e => console.log(e.message));
 
+
+axios.get(url)
+.then(({data}) => console.log(data))
+.catch(e => console.log(e.message))
+.finally(function(){
+    console.log("Fim!")
+})  ;
+
+//Qualquer acesso de hardware é async 
